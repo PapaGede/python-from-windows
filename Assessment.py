70029836f51851af3689b20355f2d027436c86ff
PapaGede <naqhouamy@gmail.com>
2019-07-10 16:04:29 +0300

    From Windows

---
+++ Assessment.py
@@ -0,0 +1,45 @@
+''''
+weight = float(input("How many pounds does your suitcase weigh? "))
+if weight >= 50:
+        print("There is a $25 charge for luggage that heavy.")
+print("Thank you for your business.")
+'''
+'''
+x = int(input("Enter your assessment score "))
+print("You entered", x ,"as your assessment \n")
+if x > 30:
+    print("Please enter a mark less than 30")    
+y = int(input("Enter your exams score "))
+print("You entered", y ,"as your exams score \n") 
+if x > 70:
+    print("Please enter a mark less than 70")
+
+sum = int
+sum = x + y
+print("Your total score for the semester is ", sum) 
+
+if sum >=70:
+    print("You had an A")
+elif sum >= 60:
+        print("You had a B")
+elif sum >= 50:
+            print("You had a C")
+elif sum >= 40:
+                print("You had a D")
+elif sum < 40:
+                    print("You failed")
+'''
+
+x= int(input("Enter the number for the multiplication table \n"))
+print("You have entered: ",x)
+y=int
+z=int
+total=int
+for y in range(1,x+1,1):
+    for z in range(12+1):
+        total=y*z
+        print(y ," * " , z , " = " ,total)
+    print("\n")
+        #All done :)
+
+
